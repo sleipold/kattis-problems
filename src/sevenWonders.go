a4dd4a00a1b1eb9d21871f56a389cf071cdd9528
Simon Leipold <simon.leipold@gmx.de>
2018-06-02 22:18:42 +0200

    solved seven wonders

---
+++ src/sevenWonders.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func mapCards(inputCards string) map[string]int {
+	cards := map[string]int{
+		"T": 0,
+		"C": 0,
+		"G": 0,
+	}
+
+	for _, card := range inputCards {
+		switch card {
+		case 'T':
+			cards["T"]++
+		case 'C':
+			cards["C"]++
+		case 'G':
+			cards["G"]++
+		}
+	}
+
+	return cards
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func calcScore(cards map[string]int) int {
+	score := 0
+	score += cards["T"]*cards["T"] + cards["C"]*cards["C"] + cards["G"]*cards["G"]
+	score += min(min(cards["T"], cards["C"]), cards["G"]) * 7
+	return score
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	inputCards, _ := reader.ReadString('\n')
+
+	cards := mapCards(inputCards)
+	score := calcScore(cards)
+	fmt.Println(score)
+}
